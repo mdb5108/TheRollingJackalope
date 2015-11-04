c3a26758d6e8b9f9d00f4a5da1344cf37db95814
Krishna Teja Salimeda <Krishna Teja Salimeda>
2015-11-04 00:17:53 -0700

    Added Gyroscope control for Player

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	private Rigidbody2D myRigidbody;
+
+	private float gyroForce = 10;
+
+	void Start ()
+	{
+		myRigidbody = GetComponent<Rigidbody2D> ();
+	}
+
+	void Update ()
+	{
+		myRigidbody.AddForce (new Vector2(Input.acceleration.x*gyroForce, Input.acceleration.y*gyroForce));
+		Debug.Log (Input.acceleration.x + "," + Input.acceleration.y);
+	}
+}
