f14ec58f2c98295cfc813a40e7e2dc5822ea0094
Minovsky <kunchengsky@gmail.com>
2015-11-04 18:40:48 -0700

    Added Character.cs and BoidsController.cs.

---
+++ Assets/Scripts/BoidsController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BoidsController : MonoBehaviour {
+
+    private float neighborRadius;
+    private List<Character> characters = new List<Character>();
+	// Use this for initialization
+	void Start () {
+	
+        neighborRadius = 300f;
+	}
+	
+    public void AddCharacter(ref Character i_character) {
+        characters.Add(i_character);
+    }
+
+    //
+    public List<Character> FindCharactersInNeighborhood(ref Character i_character) {
+        Vector2 center = (Vector2) i_character.GetComponent<Transform>().position;
+        List <Character> characterInNeighborhood = new List<Character>();
+        Character[] characters = FindObjectsOfType(typeof(Character)) as Character[];
+        foreach (Character character in characters) {
+            float distance = Vector2.Distance(center, (Vector2) character.GetComponent<Transform>().position);
+            if (distance < neighborRadius) {
+                characterInNeighborhood.Add(character);
+            }
+        }
+
+        return characterInNeighborhood;
+    }
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,18 @@
+﻿#define DEBUG
+using UnityEngine;
+using System.Collections;
+using UnityEngine.Assertions;
+
+public class Character : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+#if DEBUG
+#endif    
+	}
+}
