e886f61dff68df00bdb02181b46053bf4d5332cc
Michael Brown <gamer5108@hotmail.com>
2015-11-04 20:39:43 -0700

    Change camera to stop at bounds of map dynamically

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,23 +2,41 @@
 using System.Collections;
 
 public class CameraController : MonoBehaviour {
-	
-	public float dampTime = 0.15f;
-	private Vector3 velocity = Vector3.zero;
-	public Transform target;
-	
-	void FixedUpdate () 
-	{
-		if (target)
-		{
-			Vector3 point = GetComponent<Camera>().WorldToViewportPoint(target.position);
-			Vector3 delta = target.position - GetComponent<Camera>().ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z)); //(new Vector3(0.5, 0.5, point.z));
-			Vector3 destination = transform.position + delta;
-			//transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
-			Vector3 destPos = destination;
-			destPos.x = Mathf.Clamp(destination.x, -12.4f, 12.4f);
-			destPos.y = Mathf.Clamp(destination.y, -16.3f, 16.3f);
-			transform.position = Vector3.SmoothDamp(transform.position, destPos, ref velocity, dampTime);
-		}
-	}
+
+    public Transform borders;
+    public float dampTime = 0.15f;
+    private Vector3 velocity = Vector3.zero;
+    public Transform target;
+
+    private Camera ourCamera;
+
+    void Start()
+    {
+        ourCamera = GetComponent<Camera>();
+    }
+
+    void FixedUpdate ()
+    {
+        if (target)
+        {
+            Vector3 point = ourCamera.WorldToViewportPoint(target.position);
+            Vector3 delta = target.position - ourCamera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z));
+            Vector3 destination = transform.position + delta;
+
+            Vector2 ourCameraSize = new Vector2(ourCamera.orthographicSize*Screen.width/Screen.height, ourCamera.orthographicSize);
+            Bounds totalBounds = borders.GetComponentInChildren<Collider2D>().bounds;
+            foreach(var col in borders.GetComponentsInChildren<Collider2D>())
+            {
+                totalBounds.Encapsulate(col.bounds);
+            }
+            Vector2 mapExtents = totalBounds.max;
+
+            Vector2 limits = (mapExtents) - ourCameraSize;
+
+            Vector3 destPos = destination;
+            destPos.x = Mathf.Clamp(destination.x, -limits.x, limits.x);
+            destPos.y = Mathf.Clamp(destination.y, -limits.y, limits.y);
+            transform.position = Vector3.SmoothDamp(transform.position, destPos, ref velocity, dampTime);
+        }
+    }
 }
