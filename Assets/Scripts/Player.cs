c10239257d113944007885a5a4d4c3df2e8f108f
Krishna Teja Salimeda <krishnasalimeda@gmail.com>
2015-11-04 18:01:01 -0700

    Added Smooth Camera Movement which adjusts with a boundary,fixed player gyro movement,added static objects

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,16 +5,30 @@ public class Player : MonoBehaviour {
 
 	private Rigidbody2D myRigidbody;
 
-	private float gyroForce = 10;
+	public float gyroForce = 50;
+	public GUIText scoreText;
+	public int score = 0;
+	private GameController gameController;
 
 	void Start ()
 	{
 		myRigidbody = GetComponent<Rigidbody2D> ();
+		gameController = Camera.main.GetComponent<GameController> ();
 	}
 
-	void Update ()
+	void FixedUpdate ()
 	{
+		//Debug.Log (Input.acceleration.x + "," + Input.acceleration.y);
 		myRigidbody.AddForce (new Vector2(Input.acceleration.x*gyroForce, Input.acceleration.y*gyroForce));
-		Debug.Log (Input.acceleration.x + "," + Input.acceleration.y);
+		myRigidbody.AddForce (new Vector2(Input.GetAxis("Horizontal")*gyroForce,Input.GetAxis("Vertical")*gyroForce));
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if(other.gameObject.tag == "Friend")
+		{
+			gameController.AddScore(1);
+			Destroy(other.gameObject);
+		}
 	}
 }
