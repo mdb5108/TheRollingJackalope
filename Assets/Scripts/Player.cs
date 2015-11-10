7bf6db4d863f11586ced20371377e9dd59622e98
Michael Brown <gamer5108@hotmail.com>
2015-11-10 09:22:35 -0700

    Add Customization screen in another scene

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,6 +12,29 @@ public class Player : Character {
 
     private static readonly float MAX_SPEED = 20;
 
+    public bool respondToInput = true;
+
+    public GameObject HeadAnchor;
+    public GameObject BodyAnchor;
+    public GameObject FootAnchor;
+
+    private GameObject HeadAccessory;
+    private GameObject BodyAccessory;
+    private GameObject FootAccessory;
+
+    private static Player _instance;
+    public static Player Instance
+    {
+        get
+        {
+            if(_instance == null)
+            {
+                _instance = FindObjectOfType<Player>();
+            }
+            return _instance;
+        }
+    }
+
 	void Start ()
 	{
         //base.Start();
@@ -22,8 +45,11 @@ public class Player : Character {
 	void FixedUpdate ()
 	{
 		//Debug.Log (Input.acceleration.x + "," + Input.acceleration.y);
-		myRigidbody.AddForce (new Vector2(Input.acceleration.x*gyroForce, Input.acceleration.y*gyroForce));
-		myRigidbody.AddForce (new Vector2(Input.GetAxis("Horizontal")*gyroForce,Input.GetAxis("Vertical")*gyroForce));
+        if(respondToInput)
+        {
+            myRigidbody.AddForce (new Vector2(Input.acceleration.x*gyroForce, Input.acceleration.y*gyroForce));
+            myRigidbody.AddForce (new Vector2(Input.GetAxis("Horizontal")*gyroForce,Input.GetAxis("Vertical")*gyroForce));
+        }
 
         //Clamp player speed so bouncing doesn't get out of control
         if(myRigidbody.velocity.SqrMagnitude() > MAX_SPEED*MAX_SPEED)
@@ -38,4 +64,29 @@ public class Player : Character {
 			Destroy(other.gameObject);
 		}
 	}
+
+    public void SetHeadAccessory(string name)
+    {
+        if(HeadAccessory != null)
+            Destroy(HeadAccessory);
+        HeadAccessory = AccessoryManager.Instance.GetHeadAccessory(name);
+        HeadAccessory = (GameObject)Instantiate(HeadAccessory, HeadAccessory.transform.position, HeadAccessory.transform.rotation);
+        HeadAccessory.transform.SetParent(HeadAnchor.transform);
+    }
+    public void SetBodyAccessory(string name)
+    {
+        if(BodyAccessory != null)
+            Destroy(BodyAccessory);
+        BodyAccessory = AccessoryManager.Instance.GetBodyAccessory(name);
+        BodyAccessory = (GameObject)Instantiate(BodyAccessory, BodyAccessory.transform.position, BodyAccessory.transform.rotation);
+        BodyAccessory.transform.SetParent(BodyAnchor.transform);
+    }
+    public void SetFootAccessory(string name)
+    {
+        if(FootAccessory != null)
+            Destroy(FootAccessory);
+        FootAccessory = AccessoryManager.Instance.GetFootAccessory(name);
+        FootAccessory = (GameObject)Instantiate(FootAccessory, FootAccessory.transform.position, FootAccessory.transform.rotation);
+        FootAccessory.transform.SetParent(FootAnchor.transform);
+    }
 }
